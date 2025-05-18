2cd53d2e89f6cd8e0d8504307be876b9ecd74a2c
Pranav Mohagaokar <mohagaokarpranav@gmail.com>
2025-05-19 02:05:38 +0530

    Fix: remove basename from BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -14,10 +14,17 @@
 //   </React.StrictMode>
 // );
 
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
+import './index.css';
+import App from './App.jsx';
+
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter basename="/your-base-path">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
